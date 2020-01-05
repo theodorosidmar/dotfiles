0161eb0ef17c1a737a6f0a0f9c296b7449da2000
Sidmar Theodoro <theodorosidmar@gmail.com>
2020-01-05 18:54:53 -0300

    feat(polybar): Adds network module

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,6 +6,9 @@ echo "Polybar is starting"
 # kill
 killall -q polybar
 
+# set env variables
+DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+
 # wait
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 

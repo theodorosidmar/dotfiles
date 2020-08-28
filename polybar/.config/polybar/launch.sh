f55c717238082d22ac02ce09f1f8afa7c02f035f
Sidmar Theodoro <theodorosidmar@gmail.com>
2020-08-28 19:20:11 -0300

    feat: Adds polybar! o/

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,19 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-echo ""
-echo "Polybar is starting"
+## Add this to your wm startup file.
 
-# kill
+# Terminate already running bar instances
 killall -q polybar
 
-# set env variables
-DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# wait
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
-
-# start
-polybar main &
-
-echo ""
-echo "Polybar is launched."
+# Launch bars in every monitor
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload main &
+done

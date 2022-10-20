d72009bd63172a98e392bf6a4d886007c61ffcb5
Sidmar Theodoro <theodorosidmar@gmail.com>
2022-10-20 16:47:37 -0300

    fix(nvim): Removes deprecated method

--- vim/.config/nvim/plugin/completion.lua
+++ vim/.config/nvim/plugin/completion.lua
@@ -62,7 +62,7 @@ cmp.setup {
 
 local function config(_config)
   return vim.tbl_deep_extend("force", {
-      capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+      capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
     }, _config or {})
 end
 

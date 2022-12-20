b179d8a0a0b36deebe08cf74db2828273e1c167c
Sidmar Theodoro <theodorosidmar@gmail.com>
2022-12-20 18:05:16 -0300

    fix(vim): Fixes vim global at Lua server

--- vim/.config/nvim/after/plugin/lsp.lua
+++ vim/.config/nvim/after/plugin/lsp.lua
@@ -2,9 +2,20 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 lsp.ensure_installed({
+    'gopls',
     'tsserver',
     'eslint',
     'sumneko_lua',
+})
+
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostic = {
+                globals = { 'vim' }
+            }
+        }
+    }
 })
 
 local cmp = require('cmp')

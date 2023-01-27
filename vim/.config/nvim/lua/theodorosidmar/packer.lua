d216a87855497d9415b3ad80072e815a7da88384
sid <theodorosidmar@gmail.com>
2023-01-27 15:21:30 -0300

    chore(vim): Adds branch to lsp-zero package

--- vim/.config/nvim/lua/theodorosidmar/packer.lua
+++ vim/.config/nvim/lua/theodorosidmar/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
 
     use {
         'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
         requires = {
             -- LSP Support
             {'neovim/nvim-lspconfig'},

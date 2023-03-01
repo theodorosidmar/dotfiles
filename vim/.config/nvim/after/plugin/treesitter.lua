21e4544fc304fc0006915abd7395ea06d74cab51
sid <theodorosidmar@gmail.com>
2023-03-01 18:10:52 -0300

    feat(vim): Adds terraform to treesitter

--- vim/.config/nvim/after/plugin/treesitter.lua
+++ vim/.config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'go', 'kotlin' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'go', 'kotlin', 'terraform' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },

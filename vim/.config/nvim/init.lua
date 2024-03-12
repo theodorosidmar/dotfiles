ea593ca06d873a37b67bfeea671e0df549e155e2
sid <theodorosidmar@gmail.com>
2024-03-12 11:04:19 -0300

    fix(vim): Sets leader key at init.lua instead

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,3 +1,9 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'

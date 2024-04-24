ac03cdc9e1e984066c6f2ce341e4d90a09372c16
sid <theodorosidmar@gmail.com>
2024-04-24 14:37:53 -0300

    feat(vim): Adds ktlint to Kotlin formatters

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -432,10 +432,12 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
       format_on_save = {
-        timeout_ms = 500,
+        async = false,
+        timeout_ms = 1000,
         lsp_fallback = true,
       },
       formatters_by_ft = {
+        kotlin = { 'ktlint' },
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },

9f7371017c6a8af44eae6e3b44bf54bb0b6d771d
sid <theodorosidmar@gmail.com>
2024-07-16 13:53:37 -0300

    feat(vim): Adds bind to format & disable typescript auto format

--- vim/.config/nvim/lua/custom/plugins/autoformat.lua
+++ vim/.config/nvim/lua/custom/plugins/autoformat.lua
@@ -1,12 +1,25 @@
 return { -- Autoformat
   'stevearc/conform.nvim',
+  lazy = false,
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
   opts = {
     notify_on_error = false,
-    format_on_save = {
-      async = false,
-      timeout_ms = 1000,
-      lsp_fallback = true,
-    },
+    format_on_save = function(bufnr)
+      local disable_filetypes = { typescript = true }
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
     formatters_by_ft = {
       kotlin = { 'ktlint' },
       lua = { 'stylua' },

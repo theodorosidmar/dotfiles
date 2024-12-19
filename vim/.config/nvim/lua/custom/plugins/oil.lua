70b75a2977528a904219cdd300f1ea545db05c22
sid <theodorosidmar@gmail.com>
2024-12-19 19:16:22 -0300

    chore(vim): Changes oil icons to mini.icons

--- vim/.config/nvim/lua/custom/plugins/oil.lua
+++ vim/.config/nvim/lua/custom/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
   'stevearc/oil.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   config = function()
     require('oil').setup {
       columns = { 'icon' },

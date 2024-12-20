048f1cd26313ef31b084c835363af30de3e3f20a
sid <theodorosidmar@gmail.com>
2024-12-20 15:30:58 -0300

    chore(vim): Adds rose-pine colorscheme

--- vim/.config/nvim/lua/custom/plugins/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin-frappe'
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
---
+++ vim/.config/nvim/lua/custom/plugins/colors.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-frappe'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}

5390b6546b40860c6e16935e59d13f12f1502f29
sid <theodorosidmar@gmail.com>
2025-08-05 00:09:11 -0300

    fix(vim): Rounded winborder
    `vim.opt.winborder` rounded was messing with telescope. So I set rounded
    border to hover only

--- vim/.config/nvim/lua/custom/plugins/lsp.lua
+++ vim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -57,7 +57,9 @@ return { -- LSP Configuration & Plugins
 
         -- Opens a popup that displays documentation about the word under your cursor
         --  See `:help K` for why this keymap
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        map('K', function()
+          vim.lsp.buf.hover { border = 'rounded' }
+        end, 'Hover Documentation')
 
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header
--- vim/.config/nvim/plugin/options.lua
+++ vim/.config/nvim/plugin/options.lua
@@ -55,6 +55,3 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
-
--- Winborder for hovered documentation
-vim.opt.winborder = 'rounded'

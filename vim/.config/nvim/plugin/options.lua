3d2b60fa0ba100706263a079608b92cf319a4822
sid <theodorosidmar@gmail.com>
2025-08-04 23:26:30 -0300

    feat(vim): Sets winborder option to rounded

--- vim/.config/nvim/plugin/options.lua
+++ vim/.config/nvim/plugin/options.lua
@@ -55,3 +55,6 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Winborder for hovered documentation
+vim.opt.winborder = 'rounded'

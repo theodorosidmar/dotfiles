b258da735fce42f4ae959cbd91b6ebafada7cda3
sid <theodorosidmar@gmail.com>
2025-10-17 20:24:21 -0300

    chore(vim): Removes rose-pine colorscheme

--- vim/.config/nvim/lua/plugins/colors.lua
+++ vim/.config/nvim/lua/plugins/colors.lua
@@ -1,15 +1,9 @@
 return {
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'catppuccin-frappe'
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'catppuccin-frappe'
+    vim.cmd.hi 'Comment gui=none'
+  end,
 }

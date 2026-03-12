94fa181c2c0b5ef737d2cb8bf7ef652aa6c182b0
sid <theodorosidmar@gmail.com>
2026-03-12 15:28:54 -0300

    chore(vim): Uses catppuccin latte theme

--- vim/.config/nvim/lua/plugins/colors.lua
+++ vim/.config/nvim/lua/plugins/colors.lua
@@ -3,7 +3,8 @@ return {
   name = 'catppuccin',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'catppuccin-frappe'
+    -- vim.cmd.colorscheme 'catppuccin-frappe'
+    vim.cmd.colorscheme 'catppuccin-latte'
     vim.cmd.hi 'Comment gui=none'
   end,
 }
